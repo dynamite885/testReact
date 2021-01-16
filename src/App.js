cc8cdfc981cbb1961087039f8a0b0a69f9fad0eb
SongJH <dynamite885@naver.com>
2021-01-16 23:58:04 +0900

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // let [num, changeNum] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>{num}</h1>
+      <button onClick={()=>{changeNum(--num)}}>-1</button>
+      <button onClick={()=>{changeNum(++num)}}>+1</button> */}
+      <table border="1">
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+      </table>
     </div>
   );
+
 }
 
 export default App;
